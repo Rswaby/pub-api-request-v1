9b257122bbdaf2ac1d54be5a71d1bd96a49fcda0
Rohan Swaby <rohan_swaby51@yahoo.com>
2021-06-22 23:01:12 -0400

    comments

--- js/script.js
+++ js/script.js
@@ -3,16 +3,24 @@
  * @returns Object {results: Array(n), info: {…}}
  */
 const getNRandomUserImpl = async n => {
+    // wait for response from server with details
     let res = await fetch(`https://randomuser.me/api/?results=${n}`);
+    // if response has status code of 200/201 etc
     if(res.ok){
+        // wait for data to be resolved 
         let data = await res.json();
         return data;
     } else {
         console.error('An error has accured while fetching users: ',res.status);
     }
 }
-
+/**
+ * 
+ * @param {Object} user 
+ * @returns HTML node
+ */
 const displayUserCards = user => {
+    // deconstruct user object sent from api.
     const { name, email, location, picture, dob, phone } = user;
     const card = document.createElement('div');
     const cardImage = document.createElement('div');
@@ -35,7 +43,7 @@ const displayUserCards = user => {
 
     card.appendChild(cardImage);
     card.appendChild(cardInfo);
-
+    // when user clicks on a user card diplay model
     card.addEventListener('click',(e)=>{
         const date = new Date(dob.date)
         const body = document.querySelector('body');
@@ -43,13 +51,14 @@ const displayUserCards = user => {
         const modal = document.createElement('div');
         const button = document.createElement('button');
         const modalInfo = document.createElement('div');
-        // cosmetics
+        // add class names
         modalContainer.className ='modal-container';
         modal.className = 'modal';
         button.type = 'button';
         button.id = 'modal-close-btn';
         button.className = 'modal-close-btn';
         modalInfo.className ='modal-info-container';
+        // adding details 
         button.innerHTML = '<strong>X</strong>';
         modalInfo.innerHTML = `
             <img class="modal-img" src="${picture.thumbnail}" alt="profile picture">
@@ -70,20 +79,21 @@ const displayUserCards = user => {
         modal.appendChild(button);
         modal.appendChild(modalInfo);
         modalContainer.appendChild(modal);
+        //this will append after the script tag
         body.append(modalContainer);
 
         button.addEventListener('click',(e)=>{
             modalContainer.remove();
         })
     })
+    //return card node
     return card;
 }
-
+//make api call and display user cards 
 const displayNRandomUser = async n => {
     const gallery = document.getElementById('gallery');
     const { results }= await getNRandomUserImpl(n);
     results.forEach(user => gallery.appendChild(displayUserCards(user)));
 } 
-//TODO: paginate? 
 displayNRandomUser(12);
 
